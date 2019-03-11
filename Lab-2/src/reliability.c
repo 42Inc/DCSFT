c023f107ff8d6a932b634817eff49eb37f973650
Doshinsky <akter1997@gmail.com>
2019-03-11 12:59:22 +0700

    Almost forgot about 4th task in the lab. Fixed it

--- Lab-2/src/reliability.c
+++ Lab-2/src/reliability.c
@@ -5,11 +5,11 @@
 #include "../include/extralib.h"
 
 int main(int argc, char const *argv[]) {
-  int n[2] = {10, 16}, N[2][2] = {{8, 10}, {10, 16}}, m = 1, t[2] = {0, 24};
+  int n[2] = {10, 16}, N[2][2] = {{8, 10}, {10, 16}}, m[2] = {1, 16}, t[2] = {0, 24};
   double lambda = 0.024, mu = 0.71;
-  char* filename[2] = {"./result/1.dat", "./result/2.dat"};
+  char* filename[3] = {"./result/1.dat", "./result/2.dat", "./result/3.dat"};
 
-  for (int i = 0; i < 2; ++i) {
+  for (int i = 0; i < 3; ++i) {
     FILE* fd_out = fopen(filename[i], "w");
     if (fd_out) {
       double result = reliability_func(1, 3);
@@ -17,7 +17,6 @@ int main(int argc, char const *argv[]) {
     } else EXIT_FAILURE;
     fclose(fd_out);
   }
-
 
   return EXIT_SUCCESS;
 }
