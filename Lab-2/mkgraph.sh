cab0e17d803d8288b6d8953365724a328b70ace4
Doshinsky <akter1997@gmail.com>
2019-03-13 11:06:35 +0700

    Probably fixed the second lab... Dunno if it still works correct or not

--- Lab-2/mkgraph.sh
+++ Lab-2/mkgraph.sh
@@ -6,16 +6,23 @@ set style line 2 linetype 1 linewidth 2 linecolor rgb 'blue'
 set style line 3 linetype 1 linewidth 2 linecolor rgb 'green'
 set style line 4 linetype 1 linewidth 2 linecolor rgb 'yellow'
 set style line 5 linetype 1 linewidth 2 linecolor rgb 'pink'
+set style line 6 linetype 1 linewidth 2 linecolor rgb 'cyan'
+set style line 7 linetype 1 linewidth 2 linecolor rgb 'brown'
+set style line 8 linetype 1 linewidth 2 linecolor rgb 'black'
+set style line 9 linetype 1 linewidth 2 linecolor rgb 'grey'
+set style line 10 linetype 1 linewidth 2 linecolor rgb 'orange'
+set style line 11 linetype 1 linewidth 2 linecolor rgb 'cyan'
+set style line 12 linetype 1 linewidth 2 linecolor rgb 'brown'
+set style line 13 linetype 1 linewidth 2 linecolor rgb 'black'
 
 set border linewidth 1
 set key top right
 set grid
 set mytics 1
-set format x "%.0f"
+set format x "%.3f"
 set xlabel "Number n of elementary machines in base subsystem" font "Arial, 16"
-set format y "%.0f"
-set yrange [0:*]
-set logscale y
+set format y "%.3f"
+set yrange [*:*]
 set ylabel "Mean time between failures" font "Arial, 16"
 set xtics font "Arial, 10"
 set ytics font "Arial, 10"
@@ -24,7 +31,7 @@ set tmargin 2
 set mxtics
 
 set output './result/result_1.png'
-plot for [col=2:5] './result/1.dat' using 1:col with linespoints ls col-1 title columnheader(col)
+plot for [col=2:8] './result/1.dat' using 1:col with linespoints ls col-1 title columnheader(col)
 
 set output './result/result_2.png'
-plot for [col=2:6] './result/2.dat' using 1:col with linespoints ls col-1 title columnheader(col)
+plot for [col=2:13] './result/2.dat' using 1:col with linespoints ls col-1 title columnheader(col)
--- Lab-2/src/reliability.c
+++ Lab-2/src/reliability.c
@@ -9,31 +9,56 @@ int main(int argc, char const *argv[]) {
   //t = 0, 2, 4, …, 24 ч.
   int N = 10, m = 1;
   double lambda = 0.024, mu = 0.71;
-  for (int n = 8; n <= 10; n++)
-  {
-    printf("\nn = %d\n", n);
-    for (int t = 0; t <= 24; t += 2)
-      printf("R = %f\n", R(n, t, m, N, mu, lambda));
-  }
+
+  FILE* f_out = fopen("./result/1.dat", "w");
+	if (f_out) {
+		fprintf(f_out, "\"Number n of elementary machines in base subsystem\"    ");
+		for (int t = 0; t <= 24; t += 4)
+			fprintf(f_out, "\"t = %d/hours\"    ", t);
+		fprintf(f_out, "\n");
+
+		for (int n = 8; n <= 10; n++) {
+			fprintf(f_out, "%d    ", n);
+			for (int t = 0; t <= 24; t += 4)
+				fprintf(f_out, "%.6f    ", R(n, t, m, N, mu, lambda));
+			fprintf(f_out, "\n");
+		}
+		fclose(f_out);
+	}
 
   //N = 16; n ∈{ 10, 11, …, 16};
   N = 16;
-
-  for (int n = 10; n <= 16; n++)
-  {
-    printf("\nn = %d\n", n);
+  f_out = fopen("./result/2.dat", "w");
+  if (f_out) {
+    fprintf(f_out, "\"Number n of elementary machines in base subsystem\"    ");
     for (int t = 0; t <= 24; t += 2)
-      printf("Q = %f\n", Q_(n, N, m, mu, lambda, t));
+      fprintf(f_out, "\"t = %d/hours\"    ", t);
+    fprintf(f_out, "\n");
+
+    for (int n = 10; n <= 16; ++n) {
+      fprintf(f_out, "%d    ", n);
+      for (int t = 0; t <= 24; t += 2)
+        fprintf(f_out, "%.6f    ", Q_(n, N, m, mu, lambda, t));
+      fprintf(f_out, "\n");
+    }
+    fclose(f_out);
   }
 
-  for (m = 1; m <= 16; m++)
-  {
-    printf("\nm = %d\n", m);
-    for (int n = 11; n <= 16; n++)
-    {
-      printf("R = %f\n", R(n, 0, m, N, mu, lambda));
-      printf("S = %f\n", S(n, m, mu, lambda));
+  f_out = fopen("./result/3.dat", "w");
+  if (f_out) {
+    fprintf(f_out, "\"Number n of elementary machines in base subsystem\"    ");
+    for (m = 1; m <= 16; ++m)
+      fprintf(f_out, "\"m = %d\"    ", m);
+    fprintf(f_out, "\n");
+
+    for (int n = 11; n <= 16; ++n) {
+      fprintf(f_out, "%d    ", n);
+      for (m = 1; m <= 16; ++m)
+        fprintf(f_out, "%.6f    ", S(n, m, mu, lambda));
+      fprintf(f_out, "\n");
     }
+    fclose(f_out);
   }
+
   return EXIT_SUCCESS;
 }
