e040e66b1842478e458069dab365a5a8e30c99bc
Doshinsky <akter1997@gmail.com>
2019-04-03 22:36:00 +0700

    Added the implementation of the counting sort

--- Lab-4/include/extralib.h
+++ Lab-4/include/extralib.h
@@ -7,6 +7,7 @@
 #include <math.h>
 
 #define RANGE 32
+#define MAX_VALUE 255
 
 void NFDH(int **input_arr, int length, int n_machines);
 void FFDH();
--- Lab-4/src/extralib.c
+++ Lab-4/src/extralib.c
@@ -9,9 +9,29 @@ void FFDH() {
 }
 
 void countingSort(int **input_arr, int length) {
-  for (int i = 0; i < length; ++i) {
-    printf("%d\t", input_arr[0][i]);
-    printf("%d\n", input_arr[1][i]);
+	int output_arr[2][length];
+	int freq[MAX_VALUE];
+	memset(freq, 0, sizeof(freq));
+
+	for (int i = 0; i < length; i++)
+		freq[input_arr[0][i]]++;
+
+	int total = 0;
+	for (int i = 0; i < MAX_VALUE; i++) {
+		int oldCount = freq[i];
+		freq[i] = total;
+		total += oldCount;
+	}
+
+	for (int i = 0; i < length; i++) {
+		output_arr[0][freq[input_arr[0][i]]] = input_arr[0][i];
+    output_arr[1][freq[input_arr[0][i]]] = input_arr[1][i];
+		freq[input_arr[0][i]]++;
+	}
+
+	for (int i = 0; i < length; i++) {
+		input_arr[0][i] = output_arr[0][i];
+    input_arr[1][i] = output_arr[1][i];
   }
 }
 
